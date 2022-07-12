4327db7429468f4454120a43b5c0c68ea12c3dee
Joshua <alanda@jexbit.mx>
2022-07-12 13:03:44 -0500

    init

---
+++ src/main/java/com/apirestsap/apirestsap/ApiRestSapApplication.java
@@ -0,0 +1,12 @@
+package com.apirestsap.apirestsap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSapApplication.class, args);
+	}
+}
